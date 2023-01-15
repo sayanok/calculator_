a1b78d4f24dc5aa95e04278ae24029f861d66de5
bigen1925 <bigen1925@gmail.com>
2023-01-15 11:19:19 +0900

    fix

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -3,88 +3,97 @@ import "./App.css";
 const App: React.FC = () => {
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   const operators = ["+", "-", "×", "÷"];
-  const [currentNumber, setCurrentNumber] = useState<number>(0);
-  const [prevNumber, setPrevNumber] = useState<number>(0);
-  const [inputItem, setInputItem] = useState<string>("prevNumber");
-  const [inputOperator, setInputOperator] = useState<string>(" ");
-  const [answer, setAnswer] = useState<number | string>(0);
+  const [currentNumber, setCurrentNumber] = useState<number | null>(null);
+  const [prevNumber, setPrevNumber] = useState<number | null>(null);
+  const [numberToDisplay, setNumberToDisplay] = useState<number | string>(0);
+  const [prevNumberToDisplay, setPrevNumberToDisplay] = useState<number>(0);
+  const [inputItem, setInputItem] = useState<"prevNumber" | "currentNumber">("prevNumber");
+  const [inputOperator, setInputOperator] = useState<string>("");
+
   function onClickHandler(value: string) {
-    if (currentNumber >= 10000000 || prevNumber >= 10000000) {
-      setAnswer("ERR");
+    if (numberToDisplay.toString().length > 7) {
+      setNumberToDisplay("8桁以上は表示できないよ");
     } else {
       setNumber(value);
     }
   }
+
   function setNumber(value: string) {
     let number = "";
     if (inputItem === "prevNumber") {
-      number = prevNumber.toString() + value;
+      number = prevNumber ? prevNumber.toString() + value : value;
       setPrevNumber(parseInt(number));
+      setNumberToDisplay(parseInt(number));
+      setPrevNumberToDisplay(parseInt(number));
     } else {
-      number = currentNumber.toString() + value;
+      number = currentNumber ? currentNumber.toString() + value : value;
       setCurrentNumber(parseInt(number));
+      setPrevNumberToDisplay(parseInt(number));
+      setNumberToDisplay(parseInt(number));
     }
   }
+
   function setOperator(operator: string) {
-    if (operator === "+") {
-      if (prevNumber !== 0 && currentNumber !== 0) {
-        calculate();
-      }
-      setInputOperator("+");
-    } else if (operator === "-") {
-      if (prevNumber !== 0 && currentNumber !== 0) {
-        calculate();
-      }
-      setInputOperator("-");
-    } else if (operator === "×") {
-      if (prevNumber !== 0 && currentNumber !== 0) {
-        calculate();
-      }
-      setInputOperator("×");
-    } else if (operator === "÷") {
-      if (prevNumber !== 0 && currentNumber !== 0) {
-        calculate();
-      }
-      setInputOperator("÷");
+    if (prevNumber && currentNumber) {
+      calculate();
+    } else {
+      setInputOperator(operator);
     }
     setInputItem("currentNumber");
   }
+
   function calculate() {
     let result: number | string = 0;
-    if (inputOperator === "+") {
-      result = prevNumber + currentNumber;
-    } else if (inputOperator === "-") {
-      result = prevNumber - currentNumber;
-    } else if (inputOperator === "×") {
-      result = prevNumber * currentNumber;
-    } else if (inputOperator === "÷") {
-      result = prevNumber / currentNumber;
-    }
-    if (result >= 10000000) {
-      result = "ERR";
+    if (prevNumber && currentNumber) {
+      if (inputOperator === "+") {
+        result = prevNumber + currentNumber;
+      } else if (inputOperator === "-") {
+        result = prevNumber - currentNumber;
+      } else if (inputOperator === "×") {
+        result = prevNumber * currentNumber;
+      } else if (inputOperator === "÷") {
+        result = prevNumber / currentNumber;
+      }
+
+      if (result.toString().length > 7) {
+        result = "8桁以上は表示できないよ!";
+      } else {
+        setPrevNumber(result);
+        setCurrentNumber(0);
+      }
+      setNumberToDisplay(result);
     } else {
-      setPrevNumber(result);
-      setCurrentNumber(0);
+      setNumberToDisplay(prevNumberToDisplay);
     }
-    setAnswer(result);
   }
+
   function clear() {
     if (currentNumber !== 0) {
       setCurrentNumber(0);
-    } else if (inputOperator !== " ") {
-      setInputOperator(" ");
+      setNumberToDisplay(prevNumber ? prevNumber : 0);
+      setPrevNumberToDisplay(prevNumber ? prevNumber : 0);
+    } else if (inputOperator !== "") {
+      setInputOperator("");
+      setNumberToDisplay(prevNumberToDisplay);
+      setInputItem("prevNumber");
+      setPrevNumber(prevNumberToDisplay!);
     } else {
       setPrevNumber(0);
+      setPrevNumberToDisplay(currentNumber);
+      setNumberToDisplay(0);
       setInputItem("prevNumber");
     }
   }
+
   function allClear() {
     setCurrentNumber(0);
-    setInputOperator(" ");
+    setInputOperator("");
     setPrevNumber(0);
-    setAnswer(0);
+    setNumberToDisplay(0);
+    setPrevNumberToDisplay(0);
     setInputItem("prevNumber");
   }
+
   return (
     <div className="App">
       {numbers.map((number) => (
@@ -100,12 +109,7 @@ const App: React.FC = () => {
       <button onClick={() => calculate()}>=</button>
       <button onClick={() => clear()}>C</button>
       <button onClick={() => allClear()}>AC</button>
-      <p>
-        {prevNumber}
-        {inputOperator}
-        {currentNumber}
-      </p>
-      <p>answer: {answer}</p>
+      <p>計算内容:{numberToDisplay}</p>
     </div>
   );
 };
